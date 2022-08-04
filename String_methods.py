d9eb6f666966b2ab476b8b220b5295c521f9a027
Lokeshpatnana <109509575+Lokeshpatnana@users.noreply.github.com>
2022-08-04 08:55:26 +0530

    Add files via upload

---
+++ String_methods.py
@@ -0,0 +1,263 @@
+# a='lokesh'
+# print(a.upper())
+
+# print('lokesh'.upper())
+# print('LOKESH'.lower())
+# print('LOKESH'.casefold())
+# print('Lokesh'.isupper())
+# print('Lokesh'.islower())
+# print('LOKESH'.isupper())
+# print('lokesh'.islower())
+# print('LoKEsh'.swapcase())
+# print('loKEsh coDe'.capitalize())
+# print('loKEsh coDe'.title())
+# print('Lokesh Py'.istitle())
+# print('Lokesh py'.istitle())
+# print('Lokesh program'.startswith('N'))
+# print('Lokesh program'.startswith('L'))
+# print('Lokesh program'.startswith('Lok'))
+# print('Lokesh program'.endswith('Lok'))
+# print('Lokesh program'.endswith('am'))
+# print('Lokesh program'.endswith('gram'))
+# print('Lokesh program'.endswith('esh'))
+# print(len('lokesh'))
+# print('lokesh'.find('k'))
+# print('lokesh'.find('h'))
+# print('lokesh'.find('l'))
+# print('lokesh'.center(100))
+# txt="I love mango,mangoes are my favourite fruit"
+# print(txt.count('mango'))
+# print('lokesh'.isidentifier())
+# print('Lokesh'.isidentifier())
+# name="hEllo loKEsh"
+# print(name.center)
+# print(name.center())
+# print(name.center(50,'_'))
+# print(name.center(100))
+# name="heLLo pYthON"
+# print(type(name))
+# n=name.encode()
+# print(n)
+# print(n.decode())
+# z="heLLo PytHoN"
+# print(z.index('1'))
+# print(z.index('1',5))
+# print(z.index('0'))
+# print(z.index('0',4))
+# print(z.index('t'))
+# print(z.index(1))
+# print(z.index('b'))
+# print(z.index('n'))
+# print(z.index('P'))
+# print(z.index('L'))
+# print(z.rindex('P'))
+# r="hello pyTHOn programming"
+# print(len(r))
+# print(r.rindex('n'))
+# print(r.rindex('m'))
+# print(r.rindex('t'))
+# print(len('josh innovations'))
+# print(len('lokesh'))
+# b="_34hello"
+# print(b.isidentifier())
+# w="how is your day"
+# print(w.find('o'))
+# print(w.find('r'))
+# print(w.find('or'))
+# print(len(w))
+# print(w.rfind('o'))
+# print(w.count('h'))
+# print(w.count('i'))
+# print(w.count('o'))
+# h="python learning"
+# print(h.replace('learning', 'programming'))
+# print(h.replace('python','course'))
+# print(dir(str))
+# name='lokesh is cool'
+# print(len(name))
+# print(name.center(50))
+# print(name.center(50,'@'))
+# print(name.center(50,'#'))
+# print(name.center(50,'$'))
+# name='lalith sai'
+# print(type(name))
+# n=name.encode()
+# print(n)
+# print(n.decode())
+# t='heLLo sAi'
+# print(t.title())
+# print(t.capitalize())
+# print(t.istitle())
+# print(t.isupper())
+# print(t.islower())
+# print(t.startswith(''))
+# print(t.startswith('h'))
+# print(t.startswith('s'))
+# print(t.endswith('o'))
+# print(t.endswith(''))
+# print(t.endswith('i'))
+# v=''
+# g='           '
+# k='      l'
+# print(v.isspace())
+# print(g.isspace())
+# print(k.isspace())
+# d="hello pythonlom"
+# print(d.index('l'))
+# # print(d.index('f'))
+# print(d.index('l',6))
+# print(d.index('l',3))
+# f="      hello lokesh    .     "
+# print(f.strip())
+# print(f.lstrip())
+# print(f.rstrip())
+# g="220"
+# print(g.zfill(10))
+# f='hello'
+# print(f.zfill(6))
+# j='josh innovations'
+# print(j.upper())
+# d='hr management'
+# print(d.title())
+# a='hello world'
+# print(a.swapcase()) # here all the lower case are convert into upper case and all the upper case is convert into lower case.
+# a="hello","python","pro"
+# print(a,type(a))
+# b=' world '.join(a)
+# print(b)
+# print("Hello","python",sep=" world ")
+# c="hey"
+# h="*".join(c)
+# print(h)
+
+# g="Python Programming"
+# # print(g.removeprefix('p'))
+# # print(g.removeprefix('P'))
+# # print(g.removeprefix('Py'))
+# print(g.removesuffix('g'))
+# print(g.removesuffix('ing'))
+# print(g.removesuffix('thon'))
+
+# a="hello python"
+# print(a.isalnum())
+# b="hellopython"
+# print(b.isalnum())
+# c="hellopython89"
+# print(c.isalnum())
+# d="core python"
+# print(d.isalpha())
+# e="corepython"
+# print(e.isalpha())
+# f="corepython34"
+# print(f.isalpha())
+# g="6"
+# print(g.isalpha())
+# h="L"
+# print(h.isalpha())
+# i=""
+# print(i.isalpha())
+# j=" "
+# print(j.isalpha())
+
+# k="march python"
+# print(k.isascii())
+
+# n=chr(0)
+# p=chr(32)
+# print(n.isspace())
+# print(p.isspace())
+
+# print("hello"+chr(32)+"python")
+# print("hello"+' '+"python")
+# print("hello","python",sep=' ')
+# print("hello","python")
+
+# print(chr(32)+"lokesh")
+# print(chr(0)+"lokesh")
+
+# print(ord('a'))
+# print(ord('z'))
+# print(ord('A'))
+# print(ord('Z'))
+# print(ord('0'))
+# print(ord('9'))
+# print(ord(' '))
+# print(ord('!'))
+
+# v=bin(0)
+# print(v,type(v))
+# a=0b101
+# print(a,type(a))
+# j=0B101
+# print(j,type(j))
+
+# print(bin(24))
+# print(0b10111)
+# print(0B10111)
+
+# f=oct(24)
+# print(f,type(f))
+# print(0o30)
+# print(0O30)
+
+# h=hex(32)
+# print(h,type(h))
+# print(0x20)
+# print(0X20)
+
+# print(0xa)
+# print(0xA)
+# print(0xb)
+# print(0xB)
+# print(0xf)
+# print(0xF)
+# p="python programming"
+# print(p.swapcase())
+
+# c=5 ;d=44
+# print(d)
+# del c,d
+# print(c,c)
+
+# print("Buy this for Rs.{d} or this for Rs.{ab}".format(d=52,ab=42))
+
+# a='L\tO\tK\tI'
+# print(a.expandtabs(0))
+# print(a.expandtabs(1))
+# print(a.expandtabs(2))
+# print(a.expandtabs(3))
+# print(a.expandtabs(4))
+# print(a.expandtabs(5))
+# print(a.expandtabs(6))
+# print(a.expandtabs(7))
+# print(a.expandtabs(8))
+# print(a.expandtabs())
+# print(a.expandtabs(-1))
+
+# b='hello','python','course'
+# a='prgram'
+# print('-'.join(b))
+# print('&'.join(b))
+# print('$'.join('prgrm'))
+
+# k="hello lokesh"
+# b=k.maketrans('l','#')
+# print(k.translate(b))
+
+# l={}
+# print(l,type(l))
+# g={1:'hi',2:'bye'}
+# print(g[2])
+
+# r='hello\nlokesh'
+# print(r)
+
+# f="hello\npython\ncore\nprogramming"
+# print(f)
+# h=f.splitlines()
+# print(h,type(h))
+# # print(h[4])
+# print(h[3])
+# print(h[1])
+# print(h[0])
+# print(h[2])
